7e523a2202e045a19aa82d2a8b8116e3c5b04122
Yingjie Zhang <yingjiezhang@Yingjies-MacBook-Air.local>
2023-10-16 21:49:48 -0700

    update as

---
+++ src/Labs/a3/WorkingWithFunctions.js
@@ -0,0 +1,16 @@
+import ES5Functions from "./ES5Functions";
+import ArrowFunctions from "./ArrowFunctions";
+import ImpliedReturn from "./ImpliedReturn";
+import FunctionParenthesisAndParameters from "./FunctionParenthesisAndParameters";
+function WorkingWithFunctions() {
+    return (
+        <div>
+            <ES5Functions></ES5Functions>
+            <ArrowFunctions></ArrowFunctions>
+            <ImpliedReturn></ImpliedReturn>
+            <FunctionParenthesisAndParameters></FunctionParenthesisAndParameters>
+        </div>
+    );
+
+}
+export default WorkingWithFunctions
